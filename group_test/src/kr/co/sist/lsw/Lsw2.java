477a7e416cf2f318c521219e8023ff3d46c1ad72
user <user@DESKTOP-F1CV7BI>
2025-10-22 12:06:52 +0900

    임성우 - Lsw2 파일생성

---
+++ group_test/src/kr/co/sist/lsw/Lsw2.java
@@ -0,0 +1,11 @@
+package kr.co.sist.lsw;
+
+public class Lsw2 {
+	public static void main(String[] args) {
+		System.out.println("ㅇㅅㅇ");
+		System.out.println("ㅇㅅㅇ");
+		System.out.println("ㅇㅅㅇ");
+		System.out.println("ㅇㅅㅇ");
+		System.out.println("ㅇㅅㅇ");
+	}// main
+}// class
