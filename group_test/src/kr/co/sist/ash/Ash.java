08002752996017264bd80c3efbbe6e888cb60d95
user <user@DESKTOP-HD88287>
2025-10-22 10:55:07 +0900

    안세홍 커밋합니다

--- group_test/src/kr/co/sist/ash/Ash.java
+++ group_test/src/kr/co/sist/ash/Ash.java
@@ -3,7 +3,8 @@ package kr.co.sist.ash;
 public class Ash {
 
 	public static void main(String[] args) {
-		System.out.println("안녕하세요.");
-	}// main
+		System.out.println("Test");
+		
+	}
 
-}// class
+}
