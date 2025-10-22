ec094a201263bbc65121c5e7c72c01e3085a5259
user <user@DESKTOP-N3OI4AI>
2025-10-22 11:33:20 +0900

    1131

--- group_test/src/kr/co/sist/nkw/Ngw2.java
+++ group_test/src/kr/co/sist/nkw/Ngw2.java
@@ -3,8 +3,8 @@ package kr.co.sist.nkw;
 public class Ngw2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("개발2");
 	}
 
 }
--- group_test/src/kr/co/sist/nkw/ngw.java
+++ group_test/src/kr/co/sist/nkw/ngw.java
@@ -5,6 +5,7 @@ public class ngw {
 	public static void main(String[] args) {
 
 		System.out.println("개발끝");
+		System.out.println("개발 나경원");
 	}
 
 }
